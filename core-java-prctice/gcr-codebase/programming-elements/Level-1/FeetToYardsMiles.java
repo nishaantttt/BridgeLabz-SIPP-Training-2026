33a97d18215dc53b9bf6d20bbed657718ee27456
NISHANT SINGH <shubhamnishant04@gmail.com>
2026-06-09 10:28:43 +0530

    [Nishant] Add: added level 1 solutions

---
+++ core-java-prctice/gcr-codebase/programming-elements/Level-1/FeetToYardsMiles.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FeetToYardsMiles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter distance in feet: ");
+        double feet = input.nextDouble();
+
+        double yards = feet / 3;
+        double miles = yards / 1760;
+
+        System.out.println("Distance in feet is " + feet +
+                ", in yards is " + yards +
+                " and in miles is " + miles);
+
+        input.close();
+    }
+}
